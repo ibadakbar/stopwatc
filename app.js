dcc316039a4b28d916238178de809eb3eece8f92
ibadakbar <66198638+ibadakbar@users.noreply.github.com>
2020-07-16 13:09:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+var min = 0;
+var sec = 0;
+var msec = 0;
+var minheading = document.getElementById("min")
+var secheading = document.getElementById("sec")
+var msecheading = document.getElementById("msec")
+var interval;
+
+function timer()
+{
+    msec++
+    msecheading.innerHTML = msec;
+    if(msec >=100){
+        sec++
+        secheading.innerHTML = sec;
+        msec = 0
+    }
+    else if(sec >=60)
+    {
+        min++
+        minheading.innerHTML = min;
+        sec = 0
+    }
+}
+function start(){
+interval = setInterval(timer,10)
+btn1 = document.getElementById("btn1")
+btn1.disabled = true
+}
+function stop(){
+    clearInterval(interval)
+    btn2 = document.getElementById("btn1")
+btn2.disabled = false
+
+}
+function reset(){
+    min = 0
+    sec = 0
+    msec = 0
+    minheading.innerHTML = min
+    secheading.innerHTML = sec
+    msecheading.innerHTML = msec
+    stop()
+
+}
+function dis(){
+document.getElementById("btn1").disabled
+}
